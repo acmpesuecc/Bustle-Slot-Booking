2319ac7801258def563a401ba33e7615e5bda1ec
ad1tya24 <adityasiddanur@gmail.com>
2022-10-21 20:29:07 +0530

    QR CODE

--- Bustle.py
+++ Bustle.py
@@ -1234,7 +1234,7 @@ def checkout(): #Checkout page
     load()
     while True:
         #pdb.set_trace()
-        pchoice = input("How would you like to make your payment?:\n1.Debit card\n2.Credit card\n3.QR Code\n4.Back\n")
+        pchoice = input("How would you like to make your payment?:\n1.Debit card\n2.Credit card\n3.UPI\n4.Back\n")
         if pchoice == '1':
             while True:
                 cardchoice=input("Which card would you like to use?\n1)Mastercard\n2)Visa\n3)Back\n")
